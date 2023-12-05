9a75eae32e64c3c8766d0ae38d192932550a86ff
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-12-06 02:21:25 +0900

    moneymong-85 feat: ButtonSize 정의

---
+++ core/design-system/src/main/java/com/moneymong/moneymong/design_system/component/button/ButtonSize.kt
@@ -0,0 +1,11 @@
+package com.moneymong.moneymong.design_system.component.button
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class ButtonSize(val verticalPadding: Dp) {
+
+    LARGE(verticalPadding = 14.dp),
+    MEDIUM(verticalPadding = 12.dp),
+    SMALL(verticalPadding = 10.dp)
+}
