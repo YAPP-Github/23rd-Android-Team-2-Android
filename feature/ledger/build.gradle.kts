09b74a1bba06cdc0b52c35f4db348cad1da04903
Heon <abh0920one@gmail.com>
2023-12-06 23:55:08 +0900

    moneymong-90 feat: 장부 모듈 추가

--- feature/ledger/build.gradle.kts
+++ feature/ledger/build.gradle.kts
@@ -1,11 +1,11 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.moneymong.android.library)
     alias(libs.plugins.moneymong.android.library.compose)
+    alias(libs.plugins.moneymong.android.feature)
 }
 
 android {
-    namespace = "com.moneymong.moneymong.common"
+    namespace = "com.moneymong.moneymong.ledger"
 }
 
 dependencies {
@@ -13,11 +13,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
-
-    implementation(libs.orbit.core)
-    implementation(libs.orbit.compose)
-    implementation(libs.orbit.viewModel)
-
     testImplementation(libs.junit4)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
