ce8800822abe0c1f92aee246cd1531f93478295d
Heon <abh0920one@gmail.com>
2023-12-09 23:46:40 +0900

    moneymong-90 feat: tooltip 컴포넌트 개발

---
+++ core/design-system/src/main/java/com/moneymong/moneymong/design_system/component/tooltip/ToolTipPosition.kt
@@ -0,0 +1,6 @@
+package com.moneymong.moneymong.design_system.component.tooltip
+
+enum class MDSToolTipPosition(val paddingStart: Int, val paddingEnd: Int) {
+    Left(paddingStart = 18, paddingEnd = 0),
+    Right(paddingStart = 0, paddingEnd = 18)
+}
