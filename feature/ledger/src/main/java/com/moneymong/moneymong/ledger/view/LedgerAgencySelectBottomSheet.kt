869611825b90a94a527b6a0d14d8b48932c8b1a8
Heon <abh0920one@gmail.com>
2023-12-10 16:47:32 +0900

    moneymong-90 feat: 희직님 리뷰 반영

--- feature/ledger/src/main/java/com/moneymong/moneymong/ledger/view/LedgerAgencySelectBottomSheet.kt
+++ feature/ledger/src/main/java/com/moneymong/moneymong/ledger/view/LedgerAgencySelectBottomSheet.kt
@@ -1,35 +1,32 @@
 package com.moneymong.moneymong.ledger.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.moneymong.moneymong.ledger.view.item.LedgerGroupSelectItem
+import com.moneymong.moneymong.ledger.view.item.LedgerAgencySelectItem
 
 @Composable
-fun LedgerGroupSelectBottomSheet(
+fun LedgerAgencySelectBottomSheet(
     modifier: Modifier = Modifier,
     onClickItem: () -> Unit
 ) {
     val list = listOf(0, 1)
     Column(
-        modifier = modifier.padding(vertical = 24.dp, horizontal = 20.dp)
+        modifier = modifier.padding(vertical = 24.dp, horizontal = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(space = 12.dp)
     ) {
         list.forEachIndexed { index, item -> // TODO
-            LedgerGroupSelectItem(onClick = onClickItem)
-            if (index != list.lastIndex) {
-                Spacer(modifier = Modifier.height(12.dp))
-            }
+            LedgerAgencySelectItem(onClick = onClickItem)
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun LedgerGroupSelectBottomSheetPreview() {
-    LedgerGroupSelectBottomSheet() {}
+fun LedgerAgencySelectBottomSheetPreview() {
+    LedgerAgencySelectBottomSheet() {}
 }
