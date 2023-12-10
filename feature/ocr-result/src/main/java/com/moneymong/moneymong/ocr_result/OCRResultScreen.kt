81dc898f5ccf15d83032804f767741e30bf4a790
Heon <abh0920one@gmail.com>
2023-12-10 23:50:19 +0900

    moneymong-111 feat: ocr result topbar 개발

---
+++ feature/ocr-result/src/main/java/com/moneymong/moneymong/ocr_result/OCRResultScreen.kt
@@ -0,0 +1,40 @@
+package com.moneymong.moneymong.ocr_result
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.moneymong.moneymong.design_system.theme.Black
+import com.moneymong.moneymong.ocr_result.view.OCRResultTopbarView
+
+@Composable
+fun OCRResultScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .background(Black)
+                .padding(it)
+        ) {
+            OCRResultTopbarView(
+                modifier = Modifier.align(Alignment.TopCenter),
+                onClickBack = { /*TODO*/ },
+                onClickClose = { /*TODO*/ }
+            )
+            
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OCRResultScreenPreview() {
+    OCRResultScreen()
+}
