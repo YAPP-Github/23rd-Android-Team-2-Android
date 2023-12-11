3a9c118dc3dee9929f0486d95af82112fb9e41da
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-12-11 16:54:53 +0900

    moneymong-79 feature: AgencySearchScreen UI 구현 (소속이 존재할 경우)

--- feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/Agency.kt
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/Agency.kt
@@ -10,3 +10,48 @@ enum class AgencyType(val text: String) {
     CLUB(text = "동아리"),
     COUNCIL(text = "학생회"),
 }
+
+
+internal val mockAgencies = listOf(
+    Agency(
+        type = AgencyType.CLUB,
+        name = "나는 매우 길다.나는 매우 길다.나는 매우 길다.나는 매우 길다.나는 매우 길다.나는 매우 길다.",
+        memberCount = 10
+    ),
+
+    Agency(
+        type = AgencyType.CLUB,
+        name = "농구 동아리",
+        memberCount = 10
+    ),
+    Agency(
+        type = AgencyType.CLUB,
+        name = "축구 동아리",
+        memberCount = 564334
+    ),
+    Agency(
+        type = AgencyType.CLUB,
+        name = "배구 동아리",
+        memberCount = 13
+    ),
+    Agency(
+        type = AgencyType.CLUB,
+        name = "테니스 동아리",
+        memberCount = 321
+    ),
+    Agency(
+        type = AgencyType.COUNCIL,
+        name = "독어독문 학생회",
+        memberCount = 1
+    ),
+    Agency(
+        type = AgencyType.CLUB,
+        name = "탁구 동아리",
+        memberCount = 10
+    ),
+    Agency(
+        type = AgencyType.COUNCIL,
+        name = "컴퓨터공학과 학생회",
+        memberCount = 43
+    ),
+)
