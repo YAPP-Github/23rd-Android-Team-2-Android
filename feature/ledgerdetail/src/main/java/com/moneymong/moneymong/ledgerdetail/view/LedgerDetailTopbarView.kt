67a8d40987f42304019508df2e6604b7fbc3ffc3
Heon <abh0920one@gmail.com>
2023-12-16 01:06:25 +0900

    moneymong-109 feat: 장부 상세 topbar ui 개발

---
+++ feature/ledgerdetail/src/main/java/com/moneymong/moneymong/ledgerdetail/view/LedgerDetailTopbarView.kt
@@ -0,0 +1,72 @@
+package com.moneymong.moneymong.ledgerdetail.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.moneymong.moneymong.common.ui.noRippleClickable
+import com.moneymong.moneymong.design_system.R.*
+import com.moneymong.moneymong.design_system.theme.Gray01
+import com.moneymong.moneymong.design_system.theme.Gray07
+import com.moneymong.moneymong.design_system.theme.Gray10
+import com.moneymong.moneymong.design_system.theme.Heading1
+import com.moneymong.moneymong.design_system.theme.MMHorizontalSpacing
+
+@Composable
+fun LedgerDetailTopbarView(
+    modifier: Modifier = Modifier,
+    onClickPrev: () -> Unit,
+    onClickDelete: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Gray01),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            modifier = Modifier
+                .noRippleClickable {
+                    onClickPrev()
+                }
+                .padding(vertical = 10.dp, horizontal = MMHorizontalSpacing),
+            painter = painterResource(id = drawable.ic_chevron_left),
+            contentDescription = null,
+            tint = Gray07
+        )
+        Text(
+            modifier = Modifier.padding(vertical = 8.dp),
+            text = "상세내역",
+            style = Heading1,
+            color = Gray10
+        )
+        Icon(
+            modifier = Modifier
+                .noRippleClickable {
+                    onClickDelete()
+                }
+                .padding(vertical = 10.dp, horizontal = MMHorizontalSpacing),
+            painter = painterResource(id = drawable.ic_delete),
+            contentDescription = null,
+            tint = Gray07
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LedgerDetailTopbarPreview() {
+    LedgerDetailTopbarView(onClickPrev = { /*TODO*/ }) {
+
+    }
+}
