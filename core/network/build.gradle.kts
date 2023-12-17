f6a8b8e4ad17d55174f843ac5e6f0a76eb3cf308
Heon <abh0920one@gmail.com>
2023-12-17 19:59:44 +0900

    moneymong-131 feat: network dependency add

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson.converter)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    debugApi(libs.chucker.debug)
+    releaseApi(libs.chucker.release)
+
     testImplementation(libs.junit4)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
