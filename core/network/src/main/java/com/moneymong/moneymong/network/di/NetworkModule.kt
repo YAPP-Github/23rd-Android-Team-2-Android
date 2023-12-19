c730956cba027fd5fdc4cf9951d0e410f71c8f12
Heon <abh0920one@gmail.com>
2023-12-19 23:10:39 +0900

    moneymong-131 feat: network di module 개발

---
+++ core/network/src/main/java/com/moneymong/moneymong/network/di/NetworkModule.kt
@@ -0,0 +1,74 @@
+package com.moneymong.moneymong.network.di
+
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.moneymong.moneymong.network.util.MoneyMongTokenAuthenticator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideOkhttpClient(
+        chuckerInterceptor: ChuckerInterceptor,
+        moneymongAuthenticator: MoneyMongTokenAuthenticator
+    ): OkHttpClient {
+        val okhttpLoggingClient =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .authenticator(moneymongAuthenticator)
+            .addInterceptor(okhttpLoggingClient)
+            .addInterceptor(chuckerInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun provideChuckerInterceptor(@ApplicationContext context: Context): ChuckerInterceptor {
+        val chuckerCollector = ChuckerCollector(
+            context = context,
+            showNotification = true,
+            retentionPeriod = RetentionManager.Period.ONE_HOUR
+        )
+
+        return ChuckerInterceptor.Builder(context)
+            .collector(chuckerCollector)
+            .maxContentLength(250000L)
+            .alwaysReadResponseBody(true)
+            .createShortcut(true)
+            .build()
+    }
+
+    @Provides
+    fun provideGson(): Gson =
+        GsonBuilder()
+            .setLenient()
+            .create()
+
+    @Provides
+    @Singleton
+    fun provideRetrofitClient(
+        okHttpClient: OkHttpClient,
+        gson: Gson
+    ): Retrofit = Retrofit.Builder().apply {
+        addConverterFactory(GsonConverterFactory.create(gson))
+        client(okHttpClient)
+        baseUrl("")
+    }.build()
+
+    // TODO Api Provider
+}
