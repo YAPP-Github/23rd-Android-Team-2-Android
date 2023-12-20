c889ca6b08c7f764df3d59880d334e6a46b8b5ee
Heon <abh0920one@gmail.com>
2023-12-20 22:27:54 +0900

    moneymong-135 feat: orbit init

---
+++ feature/ocr/src/main/java/com/moneymong/moneymong/ocr/OCRViewModel.kt
@@ -0,0 +1,6 @@
+package com.moneymong.moneymong.ocr
+
+import com.moneymong.moneymong.common.base.BaseViewModel
+
+class OCRViewModel : BaseViewModel<OCRState, OCRSideEffect>(OCRState()){ // TODO Hilt
+}
