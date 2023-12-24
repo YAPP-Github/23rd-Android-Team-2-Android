eee0743960b1af7eede46dcf4ef140da1fe5781c
Heon <abh0920one@gmail.com>
2023-12-24 22:25:29 +0900

    moneymong-135 feat: 카메라 권한 모달

--- feature/ocr/src/main/java/com/moneymong/moneymong/ocr/OCRScreen.kt
+++ feature/ocr/src/main/java/com/moneymong/moneymong/ocr/OCRScreen.kt
@@ -1,5 +1,9 @@
 package com.moneymong.moneymong.ocr
 
+import android.Manifest.permission.*
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +13,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,46 +21,71 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.moneymong.moneymong.design_system.R
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.moneymong.moneymong.design_system.component.modal.MDSModal
 import com.moneymong.moneymong.design_system.theme.Heading1
 import com.moneymong.moneymong.design_system.theme.Mint03
 import com.moneymong.moneymong.design_system.theme.White
+import com.moneymong.moneymong.common.ext.hasPermission
 import com.moneymong.moneymong.ocr.view.OCRCameraPermissionDeniedView
 import com.moneymong.moneymong.ocr.view.OCRCaptureView
 import com.moneymong.moneymong.ocr.view.OCRHelperView
 import com.moneymong.moneymong.ocr.view.OCRInteractionView
 import com.moneymong.moneymong.ocr.view.OCRTopbarView
+import kotlinx.coroutines.flow.collect
+import org.orbitmvi.orbit.compose.collectAsState
 
 @Composable
 fun OCRScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: OCRViewModel = hiltViewModel()
 ) {
-    var visibleHelper by remember { mutableStateOf(true) } // TODO
-    if (false) { // TODO 카메라 권한 요청 모달
-        MDSModal(
-            icon = R.drawable.ic_camera_modal,
-            title = "카메라 접근을 허용하시겠습니까?",
-            description = "영수증 스캔을 위해 필요합니다",
-            negativeBtnText = "허용 안함",
-            positiveBtnText = "허용",
-            onClickNegative = { /*TODO*/ },
-            onClickPositive = { /*TODO*/ }
-        )
+    val state = viewModel.collectAsState().value
+    val sideEffect = viewModel.container.sideEffectFlow
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current // TODO
+    var hasCameraPermission by remember { mutableStateOf(context.hasPermission(CAMERA)) }
+    var visibleHelper by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        viewModel.visiblePermissionDialog(hasCameraPermission)
     }
 
-    if (false) { // TODO 갤러리 권한 요청 모달
+    LaunchedEffect(viewModel) {
+        sideEffect.collect {
+            when (it) {
+                OCRSideEffect.OCRMoveToPermissionSetting -> {
+                    context.startActivity(
+                        Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", context.packageName, null)
+                        )
+                    )
+                }
+                else -> {}
+            }
+        }
+    }
+
+
+
+
+
+    if (state.showPermissionDialog) {
         MDSModal(
-            icon = R.drawable.ic_photo_modal,
-            title = "갤러리 접근 권한이 필요합니다",
-            description = "설정에서 변경해주세요",
+            icon = state.modalType.icon,
+            title = state.modalType.title,
+            description = state.modalType.description,
             negativeBtnText = "확인",
             positiveBtnText = "이동",
-            onClickNegative = { /*TODO*/ },
-            onClickPositive = { /*TODO*/ })
+            onClickNegative = viewModel::onClickDialogNegative,
+            onClickPositive = viewModel::onClickDialogPositive
+        )
     }
 
     Scaffold {
@@ -66,18 +96,19 @@ fun OCRScreen(
                 .padding(it),
             contentAlignment = Alignment.Center
         ) {
-            if (false) { // TODO 카메라 접근 권한이 없다면
+            if (!hasCameraPermission) {
                 OCRCameraPermissionDeniedView(onClickRequestPermission = { /* TODO */ })
             } else {
                 OCRCaptureView()
-                if (true) { // TODO 영수증이 보일 때 숨기기
-                    Text(
-                        modifier = Modifier.align(Alignment.Center),
-                        text = "영수증의 처음과 끝이\n모두 포함되게 촬영해주세요",
-                        style = Heading1,
-                        color = Color.White,
-                        textAlign = TextAlign.Center
-                    )
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = "영수증의 처음과 끝이\n모두 포함되게 촬영해주세요",
+                    style = Heading1,
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+                if (visibleHelper) { // TODO 최초 진입 시, 혹은 도움말 아이콘 클릭 시
+                    OCRHelperView(onClickClose = { visibleHelper = !visibleHelper })
                 }
             }
             OCRTopbarView(
@@ -88,9 +119,6 @@ fun OCRScreen(
             OCRInteractionView(
                 modifier = Modifier.align(Alignment.BottomCenter),
             )
-            if (visibleHelper) { // TODO 최초 진입 시, 혹은 도움말 아이콘 클릭 시
-                OCRHelperView(onClickClose = { visibleHelper = !visibleHelper })
-            }
             if (false) { // TODO ocr loading
                 CircularProgressIndicator(
                     modifier = Modifier.size(74.dp),
--- feature/ocr/src/main/java/com/moneymong/moneymong/ocr/OCRSideEffect.kt
+++ feature/ocr/src/main/java/com/moneymong/moneymong/ocr/OCRSideEffect.kt
@@ -4,4 +4,5 @@ import com.moneymong.moneymong.common.base.SideEffect
 
 sealed class OCRSideEffect : SideEffect {
     data object OCRTakePicture : OCRSideEffect()
+    data object OCRMoveToPermissionSetting : OCRSideEffect()
 }
