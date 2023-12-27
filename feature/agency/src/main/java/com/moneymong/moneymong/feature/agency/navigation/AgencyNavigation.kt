a7eec1cd66539ea53113906ad20ccb0bd2c1e1a5
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-12-28 00:57:31 +0900

    moneymong-145 feat: 소속 Navigate 함수 추가(AgencyNavigation)

--- feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/navigation/AgencyNavigation.kt
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/navigation/AgencyNavigation.kt
@@ -3,11 +3,17 @@ package com.moneymong.moneymong.feature.agency.navigation
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.moneymong.moneymong.feature.agency.search.AgencySearchScreen
 
 const val agencyRoute = "agency_route"
+
+fun NavController.navigateAgency(navOptions: NavOptions? = null) {
+    navigate(agencyRoute, navOptions)
+}
 
 fun NavGraphBuilder.agencyScreen(padding: PaddingValues) {
     composable(route = agencyRoute) {
