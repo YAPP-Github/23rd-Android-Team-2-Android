d2efabe0505fa7a4cf1a366e67a553cdd54d967d
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-12-28 00:03:48 +0900

    moneymong-145 feat: 소속 등록 Navigation 구현

---
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/navigation/AgencyRegisterNavigation.kt
@@ -0,0 +1,29 @@
+package com.moneymong.moneymong.feature.agency.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.moneymong.moneymong.feature.agency.register.AgencyRegisterScreen
+
+const val agencyRegisterRoute = "agencyRegister_route"
+
+fun NavController.navigateAgencyRegister(navOptions: NavOptions? = null) {
+    navigate(agencyRegisterRoute, navOptions)
+}
+
+fun NavGraphBuilder.agencyRegisterScreen(
+    padding: PaddingValues,
+    navigateToComplete: () -> Unit,
+) {
+    composable(route = agencyRegisterRoute) {
+        AgencyRegisterScreen(
+            modifier = Modifier.padding(padding),
+            navigateToComplete = navigateToComplete,
+            navigateUp = { /* TODO */ }
+        )
+    }
+}
