5007725e3cefca31525751cf36f861e9cd021664
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-12-29 18:11:12 +0900

    develop fix: API Response Wrapper MoneyMongResult -> kotlin.Result 로 변경
    kotlin library 모듈인 domain 모듈에서 MoneyMongResult 를 사용하기 어렵다는 문제 발생
    -> kotlin.Result 로 많은 변경 없이 요구사항을 동일하게 가져갈 수 있다고 판단

--- core/common/src/main/java/com/moneymong/moneymong/common/error/HttpError.kt
+++ core/common/src/main/java/com/moneymong/moneymong/common/error/HttpError.kt
@@ -1,31 +1,31 @@
 package com.moneymong.moneymong.common.error
 
-sealed interface HttpError : MoneyMongError {
+sealed class HttpError : MoneyMongError() {
 
     /**
      * 400 Bad Request
      */
-    data class BadRequestError(override val message: String) : HttpError
+    data class BadRequestError(override val message: String) : HttpError()
 
     /**
      * 401 Unauthorized
      */
-    data class UnauthorizedError(override val message: String) : HttpError
+    data class UnauthorizedError(override val message: String) : HttpError()
 
     /**
      * 403 Forbidden
      */
-    data class ForbiddenError(override val message: String) : HttpError
+    data class ForbiddenError(override val message: String) : HttpError()
 
     /**
      * 404 Not Found
      */
-    data class NotFoundError(override val message: String) : HttpError
+    data class NotFoundError(override val message: String) : HttpError()
 
     /**
      * 500 Internal Server Error
      */
-    data class InternalServerError(override val message: String) : HttpError
+    data class InternalServerError(override val message: String) : HttpError()
 }
 
 fun getErrorByStatusCode(statusCode: Int, message: String): MoneyMongError {
