f56cf59a781f515f32413e934a57297ce577d224
Heon <abh0920one@gmail.com>
2024-01-01 17:55:35 +0900

    moneymong-172 feat: domain layer dto 개발

--- core/network/src/main/java/com/moneymong/moneymong/network/request/ocr/DocumentRequest.kt
+++ core/network/src/main/java/com/moneymong/moneymong/network/request/ocr/DocumentRequest.kt
@@ -1,13 +1,5 @@
 package com.moneymong.moneymong.network.request.ocr
 
-enum class DocumentFormat(val format: String) {
-    JPG("jpg"),
-    JPEG("jpeg"),
-    PNG("png"),
-    PDF("pdf"),
-    TIFF("tiff")
-}
-
 data class DocumentRequest(
     val version: String = "V2", // Clova API Document에 따라 V2로 고정합니다.
     val requestId: String,
@@ -15,7 +7,7 @@ data class DocumentRequest(
     val images: List<DocumentImage>
 ) {
     data class DocumentImage(
-        val format: String = DocumentFormat.JPEG.format,
+        val format: String,
         val data: String,
         val name: String
     )
