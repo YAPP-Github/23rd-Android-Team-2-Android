0c8ad4036f9ada4002be5df52c8c8283e5d6de5d
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-07 23:35:36 +0900

    moneymong-149 feat: 소속 찾기 State, SideEffect 생성

---
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/search/AgencySearchSideEffect.kt
@@ -0,0 +1,7 @@
+package com.moneymong.moneymong.feature.agency.search
+
+import com.moneymong.moneymong.common.base.SideEffect
+
+sealed interface AgencySearchSideEffect : SideEffect {
+    data object NavigateToRegister : AgencySearchSideEffect
+}
