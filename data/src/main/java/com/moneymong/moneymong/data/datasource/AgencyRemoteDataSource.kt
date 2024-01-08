473951465fb91c155a1ec33f7cf53bcbb8364935
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-08 23:28:18 +0900

    moneymong-149 feat: 소속 조회 Response, DataSource 구현

--- data/src/main/java/com/moneymong/moneymong/data/datasource/AgencyRemoteDataSource.kt
+++ data/src/main/java/com/moneymong/moneymong/data/datasource/AgencyRemoteDataSource.kt
@@ -1,8 +1,11 @@
 package com.moneymong.moneymong.data.datasource
 
 import com.moneymong.moneymong.network.request.agency.AgencyRegisterRequest
+import com.moneymong.moneymong.network.response.agency.AgenciesGetResponse
 
 interface AgencyRemoteDataSource {
 
     suspend fun registerAgency(request: AgencyRegisterRequest): Result<Unit>
+
+    suspend fun getAgencies(page: Int, size: Int): Result<AgenciesGetResponse>
 }
