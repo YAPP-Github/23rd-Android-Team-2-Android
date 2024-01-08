cea3dc4564feee6e269aa6b73ae301e1ee9e53e0
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-09 02:12:05 +0900

    moneymong-149 fix: 소속 등록 usecase BaseUseCase 적용

--- domain/src/main/java/com/moneymong/moneymong/domain/usecase/RegisterAgencyUseCase.kt
+++ domain/src/main/java/com/moneymong/moneymong/domain/usecase/RegisterAgencyUseCase.kt
@@ -1,14 +1,15 @@
 package com.moneymong.moneymong.domain.usecase
 
 
+import com.moneymong.moneymong.domain.base.BaseUseCase
 import com.moneymong.moneymong.domain.param.agency.AgencyRegisterParam
 import com.moneymong.moneymong.domain.repository.AgencyRepository
 import javax.inject.Inject
 
 class RegisterAgencyUseCase @Inject constructor(
     private val agencyRepository: AgencyRepository
-) {
-    suspend operator fun invoke(param: AgencyRegisterParam): Result<Unit> {
-        return agencyRepository.registerAgency(param = param)
+) : BaseUseCase<AgencyRegisterParam, Result<Unit>>() {
+    override suspend operator fun invoke(data: AgencyRegisterParam): Result<Unit> {
+        return agencyRepository.registerAgency(param = data)
     }
 }
