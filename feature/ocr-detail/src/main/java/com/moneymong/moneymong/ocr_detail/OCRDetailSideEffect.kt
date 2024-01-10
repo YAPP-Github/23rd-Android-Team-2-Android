01aa3ea0783653b1767c8dfd9cc3f99fb47d3c7c
Heon <abh0920one@gmail.com>
2024-01-10 15:56:26 +0900

    moneymong-172 feat: 장부 수정 orbit init

---
+++ feature/ocr-detail/src/main/java/com/moneymong/moneymong/ocr_detail/OCRDetailSideEffect.kt
@@ -0,0 +1,7 @@
+package com.moneymong.moneymong.ocr_detail
+
+import com.moneymong.moneymong.common.base.SideEffect
+
+sealed class OCRDetailSideEffect : SideEffect {
+
+}
---
+++ feature/ocr-detail/src/main/java/com/moneymong/moneymong/ocr_detail/OCRDetailState.kt
@@ -0,0 +1,7 @@
+package com.moneymong.moneymong.ocr_detail
+
+import com.moneymong.moneymong.common.base.State
+
+data class OCRDetailState(
+    val some: Boolean = false
+): State
