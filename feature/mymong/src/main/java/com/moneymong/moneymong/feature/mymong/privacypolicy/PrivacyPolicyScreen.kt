24055185c3a6ae94cb7213a1901d1f2658d54fe1
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-10 23:10:11 +0900

    moneymong-220 fix: 이용약관, 개인정보처리 방침 Preview 제거

--- feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/privacypolicy/PrivacyPolicyScreen.kt
+++ feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/privacypolicy/PrivacyPolicyScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.moneymong.moneymong.design_system.theme.MMHorizontalSpacing
 import com.moneymong.moneymong.design_system.theme.White
 import com.moneymong.moneymong.feature.mymong.component.MyMongInnerTopBar
@@ -33,10 +32,4 @@ fun PrivacyPolicyScreen(
             url = "https://luck-walk-147.notion.site/7f4338eda8564c1ca4177caecf5aedc8"
         )
     }
-}
-
-@Preview
-@Composable
-fun PreviewPrivacyPolicyScreen() {
-    PrivacyPolicyScreen()
 }
--- feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/termsofuse/TermsOfUseScreen.kt
+++ feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/termsofuse/TermsOfUseScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.moneymong.moneymong.design_system.theme.MMHorizontalSpacing
 import com.moneymong.moneymong.design_system.theme.White
 import com.moneymong.moneymong.feature.mymong.component.MyMongInnerTopBar
@@ -33,10 +32,4 @@ fun TermsOfUseScreen(
             url = "https://luck-walk-147.notion.site/8a382c0e511448838d2d350e16df3a95"
         )
     }
-}
-
-@Preview
-@Composable
-fun PreviewTermsOfUseScreen() {
-    TermsOfUseScreen()
 }
