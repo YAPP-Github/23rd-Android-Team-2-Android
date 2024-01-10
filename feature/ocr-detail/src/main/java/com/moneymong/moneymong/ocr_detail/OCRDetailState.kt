b4fc2b41fec5ae2ffd1299ce87d008e9dde32c25
Heon <abh0920one@gmail.com>
2024-01-10 23:50:26 +0900

    moneymong-172 feat: 장부 등록 상세 textfieldvalue onchange

--- feature/ocr-detail/src/main/java/com/moneymong/moneymong/ocr_detail/OCRDetailState.kt
+++ feature/ocr-detail/src/main/java/com/moneymong/moneymong/ocr_detail/OCRDetailState.kt
@@ -1,7 +1,17 @@
 package com.moneymong.moneymong.ocr_detail
 
+import androidx.compose.ui.text.input.TextFieldValue
 import com.moneymong.moneymong.common.base.State
+import com.moneymong.moneymong.domain.entity.ocr.DocumentEntity
+import com.moneymong.moneymong.domain.param.ledger.FundType
 
 data class OCRDetailState(
-    val some: Boolean = false
+    val document: DocumentEntity? = null,
+    val receiptImage: String = "",
+    val storeNameValue: TextFieldValue = TextFieldValue(),
+    val totalPriceValue: TextFieldValue = TextFieldValue(),
+    val paymentDateValue: TextFieldValue = TextFieldValue(),
+    val paymentTimeValue: TextFieldValue = TextFieldValue(),
+    val memoValue: TextFieldValue = TextFieldValue(text = "내용없음"),
+    val fundType: FundType = FundType.EXPENSE,
 ): State
