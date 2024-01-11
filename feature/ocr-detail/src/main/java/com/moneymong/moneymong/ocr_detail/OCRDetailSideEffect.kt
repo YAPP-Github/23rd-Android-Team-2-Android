6b003b8fe2bf26a69e30e5459e7dfb0fd7b110f6
Heon <abh0920one@gmail.com>
2024-01-12 01:04:57 +0900

    moneymong-172 feat: 증빙자료 click event handle

--- feature/ocr-detail/src/main/java/com/moneymong/moneymong/ocr_detail/OCRDetailSideEffect.kt
+++ feature/ocr-detail/src/main/java/com/moneymong/moneymong/ocr_detail/OCRDetailSideEffect.kt
@@ -3,5 +3,5 @@ package com.moneymong.moneymong.ocr_detail
 import com.moneymong.moneymong.common.base.SideEffect
 
 sealed class OCRDetailSideEffect : SideEffect {
-
+    data object OCRDetailOpenImagePicker : OCRDetailSideEffect()
 }
