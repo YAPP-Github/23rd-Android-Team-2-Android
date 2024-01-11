6dec37912562c4a4178c2a95fb5436076bdfa9bb
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-11 22:37:21 +0900

    develop fix: MDSNumberTextField 자체적으로 0 으로 시작할 수 없는 기능 제거 및 패키지 구조 변경

--- core/design-system/src/main/java/com/moneymong/moneymong/design_system/component/textfield/price/PriceType.kt
+++ core/design-system/src/main/java/com/moneymong/moneymong/design_system/component/textfield/price/PriceType.kt
@@ -1,4 +1,4 @@
-package com.moneymong.moneymong.design_system.component.textfield.util
+package com.moneymong.moneymong.design_system.component.textfield.price
 
 enum class PriceType(val symbol: String) {
     Income("+"), Expense("-")
