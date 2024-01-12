68add6120cbf906bcca3e87f5771d283fa71a14c
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-12 15:04:06 +0900

    moneymong-220 fix: HomeScreen Padding 상위(NavHost) 로 이동

--- feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/navigation/MyMongNavigation.kt
+++ feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/navigation/MyMongNavigation.kt
@@ -1,8 +1,5 @@
 package com.moneymong.moneymong.feature.mymong.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.moneymong.moneymong.feature.mymong.main.MyMongScreen
@@ -10,13 +7,11 @@ import com.moneymong.moneymong.feature.mymong.main.MyMongScreen
 const val mymongRoute = "mymong_route"
 
 fun NavGraphBuilder.mymongScreen(
-    padding: PaddingValues,
     navigateToTermsOfUse: () -> Unit,
     navigateToPrivacyPolicy: () -> Unit
 ) {
     composable(route = mymongRoute) {
         MyMongScreen(
-            modifier = Modifier.padding(padding),
             navigateToTermsOfUse = navigateToTermsOfUse,
             navigateToPrivacyPolicy = navigateToPrivacyPolicy
         )
