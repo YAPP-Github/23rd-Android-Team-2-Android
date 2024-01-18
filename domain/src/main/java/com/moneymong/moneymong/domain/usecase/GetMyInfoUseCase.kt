1379c90dbea0f589f399a2f8ab5bbab8c85cce26
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-19 00:55:22 +0900

    moneymong-221 feat: 내 정보 가져오기 UseCase 수정 및 Base 적용

--- domain/src/main/java/com/moneymong/moneymong/domain/usecase/GetMyInfoUseCase.kt
+++ domain/src/main/java/com/moneymong/moneymong/domain/usecase/GetMyInfoUseCase.kt
@@ -1,17 +1,15 @@
 package com.moneymong.moneymong.domain.usecase
 
-import com.moneymong.moneymong.domain.entity.mymong.MyInfoEntity
+import com.moneymong.moneymong.domain.base.BaseUseCase
+import com.moneymong.moneymong.domain.entity.mymong.UserEntity
+import com.moneymong.moneymong.domain.repository.UserRepository
 import javax.inject.Inject
 
-class GetMyInfoUseCase @Inject constructor() {
-    suspend operator fun invoke(): Result<MyInfoEntity> {
-        return Result.success(
-            MyInfoEntity(
-                name = "마이몽",
-                email = "mymong@yapp.com",
-                university = "마이몽대학교",
-                grade = 999
-            )
-        )
+class GetMyInfoUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) : BaseUseCase<Unit, Result<UserEntity>>() {
+
+    override suspend fun invoke(data: Unit): Result<UserEntity> {
+        return userRepository.getMyInfo()
     }
 }
