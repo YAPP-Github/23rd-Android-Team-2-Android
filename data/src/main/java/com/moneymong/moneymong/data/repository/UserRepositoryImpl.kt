756dff53da6989cff461e65f8102d088061fdc60
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-19 00:54:47 +0900

    moneymong-221 feat: UserRepository 구현

---
+++ data/src/main/java/com/moneymong/moneymong/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.moneymong.moneymong.data.repository
+
+import com.moneymong.moneymong.data.datasource.UserRemoteDataSource
+import com.moneymong.moneymong.data.mapper.user.toEntity
+import com.moneymong.moneymong.domain.entity.mymong.UserEntity
+import com.moneymong.moneymong.domain.repository.UserRepository
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userRemoteDataSource: UserRemoteDataSource
+) : UserRepository {
+
+    override suspend fun getMyInfo(): Result<UserEntity> {
+        return userRemoteDataSource.getMyInfo().map { it.toEntity() }
+    }
+}
