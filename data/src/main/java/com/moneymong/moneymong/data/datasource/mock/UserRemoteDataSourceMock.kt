672bb4126d6837f6a282921090868da149d35410
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-19 00:54:15 +0900

    moneymong-221 feat: UserRemoteDataSource 구현(Mock 포함)

---
+++ data/src/main/java/com/moneymong/moneymong/data/datasource/mock/UserRemoteDataSourceMock.kt
@@ -0,0 +1,22 @@
+package com.moneymong.moneymong.data.datasource.mock
+
+import com.moneymong.moneymong.data.datasource.UserRemoteDataSource
+import com.moneymong.moneymong.network.response.user.UserResponse
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class UserRemoteDataSourceMock @Inject constructor() : UserRemoteDataSource {
+
+    override suspend fun getMyInfo(): Result<UserResponse> {
+        delay(2000)  // for test
+        return Result.success(
+            UserResponse(
+                name = "홍길동",
+                email = "hongroaddong@mymong.com",
+                university = "길동대학교",
+                grade = 4
+            )
+        )
+//        return Result.failure(Throwable("테스트 에러"))
+    }
+}
