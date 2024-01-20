e4f031d14a7ce6def31ed0122d2cd257a21ffa25
Heon <abh0920one@gmail.com>
2024-01-20 14:53:02 +0900

    moneymong-165 feat: 장부 상세내역 조회 domain layer 개발

---
+++ domain/src/main/java/com/moneymong/moneymong/domain/entity/ledger/LedgerTransactionDetailEntity.kt
@@ -0,0 +1,13 @@
+package com.moneymong.moneymong.domain.entity.ledger
+
+data class LedgerTransactionDetailEntity(
+    val id: Int,
+    val storeName: String,
+    val amount: Int,
+    val fundType: String,
+    val description: String,
+    val paymentDate: String,
+    val receiptImageUrls: List<ReceiptImageURLEntity>,
+    val documentImageUrls: List<DocumentImageURLEntity>,
+    val authorName: String
+)
