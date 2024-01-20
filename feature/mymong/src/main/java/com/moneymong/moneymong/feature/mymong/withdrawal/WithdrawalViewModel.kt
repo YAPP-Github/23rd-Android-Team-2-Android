82764eba78d664f1bf0f4089fb2db1a49557478c
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-20 16:45:48 +0900

    moneymong-198 feat: 회원 탈퇴 viewModel 구현

---
+++ feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/withdrawal/WithdrawalViewModel.kt
@@ -0,0 +1,55 @@
+package com.moneymong.moneymong.feature.mymong.withdrawal
+
+import com.moneymong.moneymong.common.base.BaseViewModel
+import com.moneymong.moneymong.common.error.MoneyMongError
+import com.moneymong.moneymong.domain.usecase.WithdrawalUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.syntax.simple.reduce
+import javax.inject.Inject
+
+@HiltViewModel
+class WithdrawalViewModel @Inject constructor(
+    private val withdrawalUseCase: WithdrawalUseCase
+) : BaseViewModel<WithdrawalState, WithdrawalSideEffect>(WithdrawalState()) {
+
+    fun withdrawal() = intent {
+        withdrawalUseCase(data = Unit)
+            .onSuccess {
+                postSideEffect(WithdrawalSideEffect.NavigateToLogin)
+            }.onFailure {
+                reduce {
+                    state.copy(
+                        visibleErrorDialog = true,
+                        visibleWithdrawalDialog = false,
+                        errorMessage = it.message ?: MoneyMongError.UnExpectedError.message
+                    )
+                }
+            }
+    }
+
+    fun onClickAgree() = intent {
+        reduce {
+            state.copy(isAgreed = state.isAgreed.not())
+        }
+    }
+
+    fun onClickWithdrawalButton() = intent {
+        reduce {
+            state.copy(visibleWithdrawalDialog = true)
+        }
+    }
+
+    fun onNegativeClickWithdrawalDialog() = intent {
+        reduce {
+            state.copy(visibleWithdrawalDialog = false)
+        }
+    }
+
+    fun onConfirmErrorDialog() = intent {
+        reduce {
+            state.copy(visibleErrorDialog = false)
+        }
+    }
+}
