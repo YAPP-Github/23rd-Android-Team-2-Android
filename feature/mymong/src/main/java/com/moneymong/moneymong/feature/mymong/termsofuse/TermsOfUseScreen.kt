2390155749855b93dac14e5bc473573692bed087
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-25 18:19:54 +0900

    moneymong-224 feat: 마이몽 개인정보 처리 방침, 서비스 이용약관 MarkdownView 적용

--- feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/termsofuse/TermsOfUseScreen.kt
+++ feature/mymong/src/main/java/com/moneymong/moneymong/feature/mymong/termsofuse/TermsOfUseScreen.kt
@@ -1,35 +1,36 @@
 package com.moneymong.moneymong.feature.mymong.termsofuse
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.moneymong.moneymong.design_system.theme.MMHorizontalSpacing
 import com.moneymong.moneymong.design_system.theme.White
+import com.moneymong.moneymong.feature.mymong.R
 import com.moneymong.moneymong.feature.mymong.component.MyMongInnerTopBar
-import com.moneymong.moneymong.feature.mymong.component.MyMongWebView
+import com.moneymong.moneymong.feature.mymong.component.markdown.MyMongMarkdownView
 
 @Composable
 fun TermsOfUseScreen(
     modifier: Modifier = Modifier,
     navigateUp: () -> Unit
 ) {
-    Scaffold(
-        modifier = modifier,
-        topBar = {
-            MyMongInnerTopBar(
-                modifier = Modifier
-                    .background(color = White)
-                    .padding(horizontal = MMHorizontalSpacing),
-                title = "서비스 이용약관",
-                onBackClick = navigateUp
-            )
-        }
+    Column(
+        modifier = modifier
+            .background(color = White)
+            .padding(horizontal = MMHorizontalSpacing)
     ) {
-        MyMongWebView(
-            modifier = Modifier.padding(it),
-            url = "https://luck-walk-147.notion.site/8a382c0e511448838d2d350e16df3a95"
+        MyMongInnerTopBar(
+            title = "서비스 이용약관",
+            onBackClick = navigateUp
+        )
+        MyMongMarkdownView(
+            modifier = Modifier.verticalScroll(rememberScrollState()),
+            markdownText = stringResource(id = R.string.terms_of_use_content),
         )
     }
 }
