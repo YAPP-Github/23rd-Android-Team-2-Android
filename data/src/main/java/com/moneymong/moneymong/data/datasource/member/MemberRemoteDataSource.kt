c81d0ff53d2f03ccd0d2d01a0465fa18cc84d46f
기은서 <101038047+eunseo0105@users.noreply.github.com>
2024-01-28 15:06:07 +0900

    moneymong-174 fix: 병헌님 리뷰 반영

--- data/src/main/java/com/moneymong/moneymong/data/datasource/member/MemberRemoteDataSource.kt
+++ data/src/main/java/com/moneymong/moneymong/data/datasource/member/MemberRemoteDataSource.kt
@@ -4,7 +4,7 @@ import com.moneymong.moneymong.network.response.member.InvitationCodeResponse
 import com.moneymong.moneymong.network.response.member.MemberListResponse
 
 interface MemberRemoteDataSource {
-    suspend fun getInvitationCode(data: Long): Result<InvitationCodeResponse>
-    suspend fun reInvitationCode(data: Long): Result<InvitationCodeResponse>
-    suspend fun memberList(data: Long) : Result<MemberListResponse>
+    suspend fun getInvitationCode(agencyId: Long): Result<InvitationCodeResponse>
+    suspend fun reInvitationCode(agencyId: Long): Result<InvitationCodeResponse>
+    suspend fun memberList(agencyId: Long) : Result<MemberListResponse>
 }
