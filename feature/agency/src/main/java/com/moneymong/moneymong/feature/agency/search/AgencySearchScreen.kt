4d3569181fe533b8a500a6bee893bae155636b4e
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-29 22:42:15 +0900

    moneymong-124 fix: 병합과정 중 제거된 코드 복구

--- feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/search/AgencySearchScreen.kt
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/search/AgencySearchScreen.kt
@@ -15,10 +15,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -30,7 +28,6 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import com.moneymong.moneymong.design_system.R
-import com.moneymong.moneymong.design_system.component.bottomSheet.MDSBottomSheet
 import com.moneymong.moneymong.design_system.component.button.MDSFloatingActionButton
 import com.moneymong.moneymong.design_system.component.tooltip.MDSToolTip
 import com.moneymong.moneymong.design_system.component.tooltip.MDSToolTipPosition
@@ -41,13 +38,9 @@ import com.moneymong.moneymong.design_system.theme.MMHorizontalSpacing
 import com.moneymong.moneymong.design_system.theme.Red03
 import com.moneymong.moneymong.design_system.theme.White
 import com.moneymong.moneymong.feature.agency.Agency
-import com.moneymong.moneymong.feature.agency.search.component.AgencySearchBottomSheetContent
 import com.moneymong.moneymong.feature.agency.search.component.AgencySearchTopBar
 import com.moneymong.moneymong.feature.agency.search.item.AgencyItem
-import org.orbitmvi.orbit.compose.collectAsState
-import org.orbitmvi.orbit.compose.collectSideEffect
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AgencySearchScreen(
     modifier: Modifier = Modifier,
@@ -55,32 +48,7 @@ fun AgencySearchScreen(
     navigateToRegister: () -> Unit,
     navigateAgencyJoin: (agencyId: Long) -> Unit
 ) {
-    val state by viewModel.collectAsState()
     val pagingItems = viewModel.agencies.collectAsLazyPagingItems()
-
-    viewModel.collectSideEffect {
-        when (it) {
-            is AgencySearchSideEffect.NavigateToRegister -> {
-                navigateToRegister()
-            }
-
-            is AgencySearchSideEffect.NavigateToJoin -> {
-                navigateAgencyJoin(it.agencyId)
-            }
-        }
-    }
-
-    if (state.visibleBottomSheet) {
-        MDSBottomSheet(
-            onDismissRequest = viewModel::onDismissBottomSheet,
-        ) {
-            AgencySearchBottomSheetContent(
-                checkedType = state.registerType,
-                changeType = viewModel::changeRegisterType,
-                onConfirm = viewModel::onBottomSheetConfirmButtonClicked
-            )
-        }
-    }
 
     Box(
         modifier = modifier
@@ -96,7 +64,7 @@ fun AgencySearchScreen(
             AgencySearchContentView(
                 modifier = Modifier.weight(1f),
                 pagingItems = pagingItems,
-                onClickItem = viewModel::onAgencyItemClicked
+                onClickItem = navigateAgencyJoin
             )
         }
         Column(
--- feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/search/AgencySearchSideEffect.kt
+++
@@ -1,8 +0,0 @@
-package com.moneymong.moneymong.feature.agency.search
-
-import com.moneymong.moneymong.common.base.SideEffect
-
-sealed interface AgencySearchSideEffect : SideEffect {
-    data object NavigateToRegister : AgencySearchSideEffect
-    data class NavigateToJoin(val agencyId: Long) : AgencySearchSideEffect
-}
