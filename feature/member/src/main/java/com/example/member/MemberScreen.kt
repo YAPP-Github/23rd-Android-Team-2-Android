09e85ed5713a6f73eb4b486266715d69722e05a2
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-01-29 22:01:17 +0900

    moneymong-174 fix: 머지 후 import 수정

--- feature/member/src/main/java/com/example/member/MemberScreen.kt
+++ feature/member/src/main/java/com/example/member/MemberScreen.kt
@@ -3,6 +3,7 @@ package com.example.member
 import BottomSheetType
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
