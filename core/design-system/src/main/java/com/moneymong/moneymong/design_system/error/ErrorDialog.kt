e922b7857c292294533a52d71076b42bd08b59a0
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-02-06 18:16:31 +0900

    release-1.0.0 fix: moneymong-267 이슈 대응

--- core/design-system/src/main/java/com/moneymong/moneymong/design_system/error/ErrorDialog.kt
+++ core/design-system/src/main/java/com/moneymong/moneymong/design_system/error/ErrorDialog.kt
@@ -1,15 +1,15 @@
 package com.moneymong.moneymong.design_system.error
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.moneymong.moneymong.design_system.R
 import com.moneymong.moneymong.design_system.component.button.MDSButton
@@ -32,27 +33,30 @@ import com.moneymong.moneymong.design_system.component.button.MDSButtonSize
 import com.moneymong.moneymong.design_system.component.button.MDSButtonType
 import com.moneymong.moneymong.design_system.theme.Gray10
 import com.moneymong.moneymong.design_system.theme.Heading1
+import com.moneymong.moneymong.design_system.theme.MMHorizontalSpacing
 import com.moneymong.moneymong.design_system.theme.White
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ErrorDialog(
     modifier: Modifier = Modifier,
     message: String,
     onConfirm: () -> Unit
 ) {
-    AlertDialog(
-        modifier = modifier
-            .clip(RoundedCornerShape(20.dp))
-            .background(color = White),
+    Dialog(
         onDismissRequest = {},
         properties = DialogProperties(
             dismissOnBackPress = false,
-            dismissOnClickOutside = false
-        )
+            dismissOnClickOutside = false,
+            usePlatformDefaultWidth = false
+        ),
     ) {
         Column(
-            modifier = Modifier.padding(start = 22.dp, end = 22.dp, top = 24.dp, bottom = 20.dp),
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = MMHorizontalSpacing)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = White)
+                .padding(start = 22.dp, end = 22.dp, top = 24.dp, bottom = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Icon(
@@ -80,15 +84,22 @@ fun ErrorDialog(
     }
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+    device = "spec:shape=Normal,width=220,height=640, unit=dp, dpi= 480"
+)
 @Composable
 fun ErrorDialogPreview() {
     var visibleDialog by remember { mutableStateOf(true) }
 
     if (visibleDialog) {
-        ErrorDialog(
-            message = "네트워크 연결을 확인해주세요",
-            onConfirm = { visibleDialog = false }
-        )
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            ErrorDialog(
+                message = "네트워크 연결을 확인해주세요",
+                onConfirm = { visibleDialog = false }
+            )
+        }
     }
 }
