4ee3f3364b96ff572f592efcabf96ecc9df8b286
Heon <abh0920one@gmail.com>
2024-02-07 22:33:11 +0900

    release-1.0.0 fix: userId 저장 로직 순서 보장

--- feature/sign/src/main/java/com/moneymong/moneymong/feature/sign/viewmodel/LoginViewModel.kt
+++ feature/sign/src/main/java/com/moneymong/moneymong/feature/sign/viewmodel/LoginViewModel.kt
@@ -5,16 +5,11 @@ import com.moneymong.moneymong.common.base.BaseViewModel
 import com.moneymong.moneymong.domain.util.LoginCallback
 import com.moneymong.moneymong.domain.usecase.login.LoginUseCase
 import com.moneymong.moneymong.domain.usecase.login.TokenUseCase
-import com.moneymong.moneymong.domain.usecase.user.GetMyInfoUseCase
-import com.moneymong.moneymong.domain.usecase.user.SaveUserIdUseCase
-import com.moneymong.moneymong.domain.usecase.user.SaveUserNicknameUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import com.moneymong.moneymong.feature.sign.sideeffect.LoginSideEffect
 import com.moneymong.moneymong.feature.sign.state.LoginState
 import com.moneymong.moneymong.network.util.TokenCallback
 import kotlinx.coroutines.launch
-import org.orbitmvi.orbit.annotation.OrbitExperimental
-import org.orbitmvi.orbit.syntax.simple.blockingIntent
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.reduce
 import javax.inject.Inject
@@ -23,16 +18,13 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
     private val tokenUseCase: TokenUseCase,
-    private val getMyInfoUseCase: GetMyInfoUseCase,
-    private val saveUserIdUseCase: SaveUserIdUseCase,
-    private val saveUserNicknameUseCase: SaveUserNicknameUseCase
 ) : BaseViewModel<LoginState, LoginSideEffect>(LoginState()), TokenCallback {
 
     fun onLoginButtonClicked() = intent {
         viewModelScope.launch {
             loginUseCase.invoke(object : LoginCallback {
                 override suspend fun onLoginSuccess() {
-                    fetchMyInfo()
+                    //fetchMyInfo()
                     tokenUseCase.getSchoolInfo()
                         .onSuccess {
                             if(it) {
@@ -80,18 +72,6 @@ class LoginViewModel @Inject constructor(
                 )
             }
         }
-    }
-
-    @OptIn(OrbitExperimental::class)
-    fun fetchMyInfo() = blockingIntent {
-        getMyInfoUseCase(Unit)
-            .onSuccess {
-                saveUserIdUseCase(it.id.toInt())
-                saveUserNicknameUseCase(it.name)
-            }
-            .onFailure {
-                saveUserIdUseCase(0)
-            }
     }
 
     fun isLoginRequiredChanged(boolean: Boolean) = intent {
