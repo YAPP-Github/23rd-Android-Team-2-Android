dbb172927866e8fae7fe03516b4243c3d3afa4bb
Heon <abh0920one@gmail.com>
2024-02-07 22:01:54 +0900

    release-1.0.0 fix: 소속 바텀싯 스크롤 적용

--- feature/ledger/src/main/java/com/moneymong/moneymong/ledger/view/LedgerAgencySelectBottomSheet.kt
+++ feature/ledger/src/main/java/com/moneymong/moneymong/ledger/view/LedgerAgencySelectBottomSheet.kt
@@ -3,8 +3,19 @@ package com.moneymong.moneymong.ledger.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.moneymong.moneymong.domain.entity.agency.MyAgencyEntity
@@ -17,8 +28,12 @@ fun LedgerAgencySelectBottomSheet(
     agencyList: List<MyAgencyEntity>,
     onClickItem: (agencyId: Int) -> Unit
 ) {
+    val scrollState = rememberScrollState()
+
     Column(
-        modifier = modifier.padding(vertical = 24.dp, horizontal = 20.dp),
+        modifier = modifier
+            .verticalScroll(scrollState)
+            .padding(vertical = 24.dp, horizontal = 20.dp),
         verticalArrangement = Arrangement.spacedBy(space = 12.dp)
     ) {
         agencyList.forEachIndexed { index, item ->
