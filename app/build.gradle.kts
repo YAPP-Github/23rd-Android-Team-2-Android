8153fa4b2823d185204495045ffce13e8941d4fb
Heon <abh0920one@gmail.com>
2024-02-12 23:19:37 +0900

    chore: 버전 코드 업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "com.moneymong.moneymong"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
